3aaa056dfc3837169a888f3530a1d90e60c65234
Michael Tugby <mtugby@Michaels-MacBook-Pro.local>
2017-12-06 10:23:38 +0000

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-toolbar app color="primary" fixed>
+      <v-toolbar-side-icon class="white--text"></v-toolbar-side-icon>
+      <v-toolbar-title>Test</v-toolbar-title>
+    </v-toolbar>
+  </v-app>
+</template>
+
+<script>
+import VToolbar, { VToolbarSideIcon, VToolbarTitle } from 'vuetify/es5/components/VToolbar';
+export default {
+  components: {
+    VToolbar,
+    VToolbarSideIcon,
+    VToolbarTitle,
+  },
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,56 @@
+const nodeExternals = require('webpack-node-externals')
+const resolve = (dir) => require('path').join(__dirname, dir)
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'nuxt-vuetify-test',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+    ]
+  },
+  plugins: ['~/plugins/vuetify.js'],
+  css: [
+    '~/assets/style/app.styl'
+  ],
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    babel: {
+      plugins: [
+        ["transform-imports", {
+          "vuetify": {
+            "transform": "vuetify/es5/components/${member}",
+            "preventFullImport": true
+          }
+        }]
+      ]
+    },
+    vendor: [
+      '~/plugins/vuetify.js'
+    ],
+    extractCSS: true,
+    extend (config, ctx) {
+      if (ctx.isServer) {
+        config.externals = [
+          nodeExternals({
+            whitelist: [/^vuetify/]
+          })
+        ]
+      }
+    }
+  }
+}
